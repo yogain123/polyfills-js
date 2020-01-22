d29e7068c733e2bf85d5e0e1dfaa3fb5f3971aed
yogain123 <yogendrasaxena56@gmail.com>
2020-01-22 14:36:11 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,42 @@
+// polyfill for 'every'
+Array.prototype.every = function(fn) {
+  for (const item of this) {
+    if (!fn(item)) return false;
+  }
+  return true;
+};
+
+//polyfill for 'some'
+Array.prototype.some = function(fn, acc) {
+  for (const item of this) {
+    if (fn(item)) return true;
+  }
+  return false;
+};
+
+//polyfill for 'reduce' function
+Array.prototype.reduce = function(fn, acc = 0) {
+  let result;
+  for (let i = 0; i < this.length; i++) {
+    result = fn(acc, this[i], i, this);
+    acc = result;
+  }
+  return result;
+};
+
+//polyfill for 'async forEach' function
+Array.prototype.asyncForEach = async function(fn) {
+  for (let i = 0; i < this.length; i++) {
+    await fn(this[i], i, this);
+  }
+};
+
+Function.prototype.myBind = function(data) {
+  return (age1, age2) => {
+    return this.call(data, age1, age2);
+  };
+};
+
+Function.prototype.myCall = function(data, ...rest) {
+  return this.apply(data, rest);
+};
