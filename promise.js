c1b2e734c28aa0951d10ccf21684f1d7b4230f79
yogendra saxena <yogendrasaxena56@gmail.com>
2020-02-29 22:35:52 +0530

    Create promise.js

---
+++ promise.js
@@ -0,0 +1,37 @@
+let p1 = new Promise((resolve, reject) => {
+    resolve(1);
+});
+
+let p2 = new Promise((resolve, reject) => {
+    resolve(2);
+});
+
+let p3 = new Promise((resolve, reject) => {
+    reject(3);
+});
+
+
+//Promise.settleAll polyfill
+Promise.allSettledPolyFill = async function(promiseArr) {
+    let resarr = [];
+    let counter = 0;
+    for (const item of promiseArr) {
+        try {
+            resarr.push( {status:"fulfilled", value:await item});
+            counter++;   
+        } catch (error) { 
+            resarr.push({status:"rejected", value:error}) 
+        }
+    }
+    return Promise.resolve(resarr)
+}
+
+Promise.allSettledPolyFill([p1,p2,p3]).then((res) => {
+    console.log(res);
+})
+
+/*
+[ { status: 'fulfilled', value: 1 }, 
+  { status: 'fulfilled', value: 2 }, 
+  { status: 'rejected', value: 3 } ] 
+*/
